ec23ae2c83b4dc18dbcf65ec6bc178cbcbd93eb6
Alejandro Alvarez <aalvarez1603@gmail.com>
2025-11-19 18:44:05 +0100

    ejer

---
+++ ejercicioparte1/ejercicio11.2.py
@@ -0,0 +1,11 @@
+#Escriba un programa que lee dos números y muestra el mayor
+
+n1= float(input("Comparador 2 numeros"))
+
+n2= float(input("Comparador 2 numeros"))
+
+
+if n1 > n2:
+    print("{n1} es el mayor")
+else:
+    print("{n2} es el mayor")
---
+++ ejercicioparte1/ejercicio12.2.py
@@ -0,0 +1,9 @@
+#Escriba un programa que lea un número y dice si es positivo o negativo, 
+# consideramos el cero como positivo
+
+n1= float(input("Negativo o Positivo"))
+
+if n1 >= 0:
+    print("{n1} es positivo")
+else:
+    print("{n2} es negativo")
