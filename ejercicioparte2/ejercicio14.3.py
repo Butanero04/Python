ce41ec240635232e29a563c687b30480f4685bb7
Alejandro Alvarez <aalvarez1603@gmail.com>
2025-11-20 20:45:10 +0100

    a

---
+++ ejercicioparte2/ejercicio14.3.py
@@ -0,0 +1,5 @@
+altura=int(input("Intruce la altura de la pirámide: "))
+
+if altura<=0:
+    print("ERROR: Eres muy tonto, solamente son validos números enteros")
+else:
---
+++ ejercicioparte2/ejercicio15.3.py
@@ -0,0 +1,10 @@
+a = int(input("Dame la altura de tu piramide inversa"))
+
+
+if a <= 0:
+    print("ERROR: ERES TONTO DEBE DE SER MAYOR QUE 0")
+else:
+    for i in range (1,a+1):
+        
+        #construir especios
+        for j in range(i-1,altura)
